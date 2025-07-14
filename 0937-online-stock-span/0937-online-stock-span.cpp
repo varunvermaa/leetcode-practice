570046e1b66e780b902e6d0cb26297c1ac9047f8
Varun Verma <155096244+varunvermaa@users.noreply.github.com>
2025-07-14 21:22:07 +0530

    Time: 14 ms (98.13%), Space: 92.1 MB (28.3%) - LeetHub

---
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -0,0 +1,29 @@
+class StockSpanner {
+public:
+
+    stack<pair<int,int>> s;
+   
+
+    StockSpanner() {
+        
+    }
+    
+    int next(int p) {
+        
+        int span = 1;
+
+        while(!s.empty() && s.top().first <= p){
+            span+= s.top().second;
+            s.pop();
+        }
+        s.push({p, span});
+        return span;
+
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
