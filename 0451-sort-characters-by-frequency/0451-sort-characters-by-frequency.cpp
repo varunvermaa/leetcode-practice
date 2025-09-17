31731da812062d0367a4209f43aa3c5a434abc32
Varun Verma <155096244+varunvermaa@users.noreply.github.com>
2025-09-17 11:48:06 +0530

    Time: 0 ms (100%), Space: 12.9 MB (12.21%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        vector<pair<int,char>> hash ('z'+1, {0,0});
-        for(char a : s){
-            hash[a] = {hash[a].first+1, a};
+        vector<pair<int, char>> h('z'+1, {0,0});
+        string ans;
+        for(int i=0; i<s.length(); i++){
+            h[s[i]]={h[s[i]].first+1, s[i]};
         }
-
-        sort(hash.begin(), hash.end());
-        string ans="";
-        for(auto i : hash){
-            ans = string(i.first, i.second) + ans;
+        sort(h.begin(), h.end());// if you sorted in ascending order, you will have to prepend the string in the next for loop.
+        for(auto i: h){
+            ans = string(i.first, i.second) + ans; // most imp part... the string is prepended to the ans string.
         }
         return ans;
-
     }
 };
